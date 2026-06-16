5efacb5978fb603cc899724b2cd0540236456420
jenjirahuaisai <Jenjira.huaisai@stenden.nhlstenden.com>
2026-06-16 21:09:24 +0200

    Add breeding pigeon registration page and update dashboard UI

--- Website/Php/dashboard.php
+++ Website/Php/dashboard.php
@@ -224,10 +224,10 @@
                                 Egg</span></a>
                         <a href="./register-new-youngster.php"><img src="../images/dashboard-icon/add.png"
                                 alt=""><span>Register Youngster</span></a>
-                        <a href="../add-race-result.html"><img src="../images/dashboard-icon/add.png" alt=""><span>Add Race
-                                Result</span></a>
+                        <a href="../add-new-breeding-pigeon.html"><img src="../images/dashboard-icon/add.png" alt=""><span>Add Breeding Pigeon</span></a>
                         <a href="#"><img src="../images/dashboard-icon/add.png" alt=""><span>Health Record</span></a>
-                        <a href="#"><img src="../images/dashboard-icon/add.png" alt=""><span>Add Calendar</span></a>
+                        <a href="./add-race-result.html"><img src="../images/dashboard-icon/add.png" alt=""><span>Add Race
+                                Result</span></a>
                     </div>
                 </article>
             </section>
@@ -264,7 +264,7 @@
                     <div class="alert red">
                         <img src="../images/dashboard-icon/warning.png" alt="">
                         <p><strong>Low survival rate alert</strong><br>Pair 1 has a survival rate below 70%</p>
-                        <button>View Pair</button>
+                        <button class="alert-button">View Pair</button>
                     </div>
 
                     <div class="alert orange">
